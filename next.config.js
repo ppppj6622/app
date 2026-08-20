2df804e636d3c0de344888b0cb52c2662bbcf335
dhdu8486 <dhdu8486@example.com>
2026-08-20 08:35:44 +0800

    fix: add output export for Vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
-  trailingSlash: true
+  trailingSlash: true,
+  output: 'export'
 }
 module.exports = nextConfig
